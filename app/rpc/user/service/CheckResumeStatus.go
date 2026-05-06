bb8aa1a45a8161464bca8d074f555b9ad641c80d
WangZhonghao <3290120053@qq.com>
2026-05-06 20:20:41 +0800

    chore: 修改包名及补上上一次user的rpc部分为提交部分

--- app/rpc/user/service/CheckResumeStatus.go
+++ app/rpc/user/service/CheckResumeStatus.go
@@ -3,7 +3,12 @@ package service
 import (
 	"Goffer/app/rpc/user/svc"
 	"Goffer/kitex_gen/user"
+	"Goffer/pkg/errno"
 	"context"
+	"errors"
+	"fmt"
+
+	"gorm.io/gorm"
 )
 
 type CheckStatusService struct {
@@ -19,7 +24,10 @@ func NewCheckStatusService(svc *svc.ServiceContext) *CheckStatusService {
 func (s *CheckStatusService) CheckResumeStatus(ctx context.Context, req *user.CheckResumeStatusReq) (int, error) {
 	status, err := s.svc.DB.GetResumeStatus(ctx, req.ResumeId, req.UserId)
 	if err != nil {
-		return 0, err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return 0, errno.ResumeNotFoundErr
+		}
+		return 0, fmt.Errorf("query resume status failed: %w", err)
 	}
 
 	return status, nil
