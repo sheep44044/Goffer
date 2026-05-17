badd0ec7046366884dd2f0a5e90b1ed2b8476558
WangZhonghao <3290120053@qq.com>
2026-05-17 17:33:30 +0800

    feat: 优化api层的错误处理和日志返回

---
+++ app/api/handlers/pack/response.go
@@ -0,0 +1,24 @@
+package pack
+
+import (
+	"Goffer/pkg/errno"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+type Response struct {
+	Code    int64       `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+// SendResponse 将 errno.ErrNo 统一序列化为前端 JSON 响应
+func SendResponse(c *app.RequestContext, err error, data interface{}) {
+	Err := errno.ConvertErr(err)
+	c.JSON(consts.StatusOK, Response{
+		Code:    Err.ErrCode,
+		Message: Err.ErrMsg,
+		Data:    data,
+	})
+}
